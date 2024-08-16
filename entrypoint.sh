f8efab3757a5ea4a30d32857bb9f91b6dd67c11b
José Lino santos García <191011022@acatlan.tecnm.mx>
2024-08-16 09:48:53 -0600

    first commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 echo 'Running collectsatic...'
-python manage.py collectstatic --no-input --settings=core.settings.production
+python manage.py collectstatic --no-input 
 
 
 echo 'apliying migrations...'
-python manage.py wait_for_db --settings=core.settings.production
-python manage.py migrate --settings=core.settings.production
-python manage.py migrate --run-syncdb --settings=core.settings.production
+python manage.py wait_for_db 
+python manage.py migrate 
+python manage.py migrate 
 
 echo 'Running server...'
-gunicorn --env DJANGO_SETTINGS_MODULE=core.settings.production core.wsgi:application --bind 0.0.0.0:8000
+gunicorn --env DJANGO_SETTINGS_MODULE=core core.wsgi:application --bind 0.0.0.0:8000
