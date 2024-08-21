f35557b6ea482067c084209b2b93c3cc9ac14f57
José Lino santos García <191011022@acatlan.tecnm.mx>
2024-08-21 14:12:00 -0600

    MOD:PROD

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ python manage.py wait_for_db --settings=core.settings.production
 python manage.py migrate --settings=core.settings.production
 
 echo 'Running server...'
-gunicorn --env DJANGO_SETTINGS_MODULE=core.settings.production core.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn --env DJANGO_SETTINGS_MODULE=core.settings.production core.wsgi:application --bind 0.0.0.0:8000
