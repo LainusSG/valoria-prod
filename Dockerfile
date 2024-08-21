d66e9255c1327acb31966c4955927e7552fdb297
José Lino santos García <191011022@acatlan.tecnm.mx>
2024-08-21 16:42:01 -0600

    MOD:PROD

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,15 @@
-FROM python:3.10
-WORKDIR /code
-COPY . /code/
-RUN python -m pip install --upgrade pip
+FROM --platform=linux/amd64 python:3.12.1-bookworm as builder
+
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR /app
+
+RUN pip install --upgrade pip
+
+COPY ./requirements.txt ./
+
 RUN pip install -r requirements.txt
+
+COPY ./ ./
+
+CMD ["sh", "entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-echo 'Running collecstatic...'
+echo 'Running collectsatic...'
 python manage.py collectstatic --no-input --settings=core.settings.production
 
-echo 'Applying migrations...'
+
+echo 'apliying migrations...'
 python manage.py wait_for_db --settings=core.settings.production
 python manage.py migrate --settings=core.settings.production
+python manage.py migrate --run-syncdb --settings=core.settings.production
 
 echo 'Running server...'
 gunicorn --env DJANGO_SETTINGS_MODULE=core.settings.production core.wsgi:application --bind 0.0.0.0:8000
